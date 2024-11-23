88571f68624384d9cf6713760a57df2574c20f6c
dhanush.rajn <dhanush.rajn@lge.com>
2024-11-23 16:15:19 +0000

    Resolve c-flags issue for webos-qemux
    :Release Notes:
    Resolve c-flags issue for webos-qemux
    
    :Detailed Notes:
    Resolved c-flags issue by typecasting to
    va_list*
    
    :Testing Performed:
    Tested locally
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [WRR-9888] To resolve cflags for connectivity components
    
    Change-Id: I7425eca9ef0de55eb40f3fa76de61a8aecae4258

--- src/lunaservice_utils.c
+++ src/lunaservice_utils.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2018 LG Electronics, Inc.
+// Copyright (c) 2012-2024 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -241,7 +241,7 @@ static bool LSMessageParseToNative_valist(LSMessage *message, jvalue_ref *parsed
 		return false;
 	}
 
-	error = json_convert_to_native_valist(*parsedObj, &args);
+	error = json_convert_to_native_valist(*parsedObj, (va_list *)&args);
 
 	if (error)
 	{
